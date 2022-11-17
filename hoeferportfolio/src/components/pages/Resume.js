fa6f6beeb4b669463295b48484223be832c919c0
Gayle Hoefer <gaylehoefer@gmail.com>
2022-11-16 19:22:43 -0700

    added folder setup

---
+++ hoeferportfolio/src/components/pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Contact() {
+    return (
+        <div>
+            <h1>Resume</h1>
+            <p>
+                Resume place holder
+            </p>
+        </div>
+    );
+}
