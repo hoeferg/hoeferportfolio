546404b90e833a6e108fd33e3a8086e448fdc966
Gayle Hoefer <gaylehoefer@gmail.com>
2022-11-19 07:38:33 -0700

    updated react to react 18 in application

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,45 +7,45 @@ export default function Home() {
         <div>
             <section>
                 <h1>Gayle Hoefer</h1>
-                <div id="id-card" class="w3-center w3-container w3-theme-l2 w3-col s11 l2 w3-round-large w3-card">
-                    <img class="w3-col s10 w3-margin-top w3-round" src="./assets/images/IMG_0372.jpg" alt="" style="float: none;" />
-                    <figcaption class="w3-large w3-col s12" />
+                <div id="id-card" className="{{w3-center w3-container w3-theme-l2 w3-col s11 l2 w3-round-large w3-card}}">
+                    <img className="{{w3-col s10 w3-margin-top w3-round}}" src="./assets/images/IMG_0372.jpg" alt="" style="float: none;" />
+                    <figcaption className="{{w3-large w3-col s12}}" />
                     <h2>Gayle Hoefer</h2>
                 </div>
             </section>
 
-            <section id="welcome" class=" w3-container w3-theme-l3 w3-col s11 l9 w3-round-large">
-                <article class="w3-margin-top w3-container w3-margin-bottom w3-theme-d2 w3-col s12 l12 w3-round-xlarge w3-card-4" >
-                    <h2 class="w3-center">
+            <section id="welcome" className="{{w3-container w3-theme-l3 w3-col s11 l9 w3-round-large}}">
+                <article className="{{w3-margin-top w3-container w3-margin-bottom w3-theme-d2 w3-col s12 l12 w3-round-xlarge w3-card-4}}" >
+                    <h2 className="{{w3-center}}">
                         Welcome!</h2>
 
-                    <p className="welcome-p">
+                    <p className="{{welcome-p}}">
                         Hello, my name is Gayle Hoefer and welcome to my online portfolio. I am creating new web
                         applications as we speak, so check back often for new developments.
                     </p>
-                    <p className="welcome-p">
+                    <p className="{{welcome-p}}">
                         I am currently in the University of Arizona full-stack development boot camp. The goal of this site
                         is to display and share my progress as I get through this fast paced curriculum. As I progress I
                         will update this site to include new code that I have learned.
                     </p>
-                    <p className="welcome-p">
+                    <p className="{{welcome-p}}">
                         Already the boot camp has been a great experience and I am now very familiar (working every day)
                         with the following platforms and technologies:
-                        <ul id="welcome-list">
-                            <li>GitHub</li>
-                            <li>Git Bash</li>
-                            <li>HTML5</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                            <li>Node.js</li>
-                        </ul>
+
+                        <li>GitHub</li>
+                        <li>Git Bash</li>
+                        <li>HTML5</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>Node.js</li>
+
                     </p>
-                    <p className="welcome-p">
+                    <p className="{{welcome-p}}">
                         I am finding that I like to take a design first approach to building a website. I do realize that a
                         site that does not function is not worth visiting but I find that when function following design can
                         make for less refactoring to allow for the style desired.
                     </p>
-                    <p className="welcome-p">
+                    <p className="{{welcome-p}}">
                         Please take a look at some of my tinkering below
                     </p>
                 </article>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import * as ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
